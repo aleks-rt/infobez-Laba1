589de12b72c089d77d1304eb7fcc81d3a6e320a7
aleks-rt <40232973+aleks-rt@users.noreply.github.com>
2022-09-24 12:19:36 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
+function readFile(object) {
+  var file = object.files[0]
+  var reader = new FileReader()
+  reader.onload = function() {
+    document.getElementById('out').innerHTML = reader.result
+    let a = document.getElementById('out').innerHTML;
+    console.log(a);
+
+  }
+  reader.readAsText(file)
+}
+
+
 const ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
 function getEncodedAlphabet(alphabet, slip, keyWord) {
@@ -29,26 +42,27 @@ function transform(alphabet, encodedAlphabet, text) {
   return result;
 }
 
-function encode(text, slip, keyWord) {
+function encode(a, slip, keyWord) {
   let encodedAlphabet = getEncodedAlphabet(ALPHABET, slip, keyWord);
 
-  return transform(ALPHABET, encodedAlphabet, text);
+  return transform(ALPHABET, encodedAlphabet, a);
 }
 
-function decode(text, slip, keyWord) {
+function decode(a, slip, keyWord) {
   let encodedAlphabet = getEncodedAlphabet(ALPHABET, slip, keyWord);
 
-  return transform(encodedAlphabet, ALPHABET, text);
+  return transform(encodedAlphabet, ALPHABET, a);
 }
 
 
 
 document.getElementById("text").addEventListener("input", function() {
+  let a = document.getElementById('out').innerHTML;
   let text = this.value;
   let slip = document.getElementById("slip").value;
   let keyWord = document.getElementById("key-word").value;
 
-  let encodedText = encode(text, slip, keyWord);
+  let encodedText = encode(a, slip, keyWord);
   let decodedText = decode(encodedText, slip, keyWord);
 
   document.getElementById("output-encoded").textContent = encodedText;
@@ -56,11 +70,12 @@ document.getElementById("text").addEventListener("input", function() {
 });
 
 document.getElementById("slip").addEventListener("input", function() {
+  let a = document.getElementById('out').innerHTML;
   let text = document.getElementById("text").value;
   let slip = this.value;
   let keyWord = document.getElementById("key-word").value;
 
-  let encodedText = encode(text, slip, keyWord);
+  let encodedText = encode(a, slip, keyWord);
   let decodedText = decode(encodedText, slip, keyWord);
 
   document.getElementById("output-encoded").textContent = encodedText;
@@ -68,11 +83,26 @@ document.getElementById("slip").addEventListener("input", function() {
 });
 
 document.getElementById("key-word").addEventListener("input", function() {
+  let a = document.getElementById('out').innerHTML;
   let text = document.getElementById("text").value;
   let slip = document.getElementById("slip").value;
   let keyWord = this.value;
 
-  let encodedText = encode(text, slip, keyWord);
+  let encodedText = encode(a, slip, keyWord);
+  let decodedText = decode(encodedText, slip, keyWord);
+
+  document.getElementById("output-encoded").textContent = encodedText;
+  document.getElementById("output-decoded").textContent = decodedText;
+});
+
+
+document.getElementById("button").addEventListener('click', function() {
+  let a = document.getElementById('out').innerHTML;
+  console.log(a);
+  let slip = document.getElementById("slip").value;
+  let keyWord = document.getElementById("key-word").value;
+
+  let encodedText = encode(a, slip, keyWord);
   let decodedText = decode(encodedText, slip, keyWord);
 
   document.getElementById("output-encoded").textContent = encodedText;
